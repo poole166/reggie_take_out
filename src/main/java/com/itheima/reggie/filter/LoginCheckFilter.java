c13eb04800e41ed6de5b8eee8b4b2e7aeb3dda02
“poole166” <jinitaimei@giao.com>
2025-08-17 18:08:53 +0800

    path to linux

--- src/main/java/com/itheima/reggie/filter/LoginCheckFilter.java
+++ src/main/java/com/itheima/reggie/filter/LoginCheckFilter.java
@@ -39,7 +39,12 @@ public class LoginCheckFilter implements Filter{
                 "/front/**",
                 "/common/**",
                 "/user/sendMsg",
-                "/user/login"
+                "/user/login",
+                "doc.html",
+                "doc.html",
+                "web jars/**",
+                "/swagger-resources",
+                "/v2/api-docs"
         };
 
         //2、判断本次请求是否需要处理
